9fc14de435ca0a6d897ff45becbbc9db042fe543
yuxiaoyang03 <yuxiaoyang03@baidu.com>
2019-10-09 19:59:46 +0800

    完成从本地创建模板或网上拉模板两种方式

--- init-demo/index.js
+++ init-demo/index.js
@@ -1,24 +1,27 @@
-const program  =require('commander');
+const program  = require('commander');
 
 program
-  .command('create-mycli-app <type> [projectName] [otherParams]')
+  .command('create-mycli-app <type> [fileType] [projectName]')
   .alias('mycli')
   .description('init a new project')
   .action(callback)
 
-function callback(type,projectName,otherParams){
-  console.log('type', type, projectName, otherParams);
+function callback(type,fileType,projectName){
+  console.log(type,fileType,projectName)
   switch(type) {
       case 'download':
-        // 从仓库下载模版文件
+        // 从仓库下载模版文件 mycli download [component|route] projectName
+        // node index.js mycli download component myDownload
         const downloadFunc = require('./download.js');
-        downloadFunc(projectName);
+        downloadFunc(fileType);
         break;
 
       case 'create':
-        // 命令行创建模板文件 mycli create 
+        // 命令行创建模板文件 mycli create [component|route] projectName
+        // node index.js create-mycli-app create component myapp 创建方法1
+        // node index.js mycli create component myapp 创建方法2
         const createFunc = require('./create.js');
-        createFunc(projectName, otherParams);
+        createFunc(fileType,projectName);
         break;
 
       default: 
